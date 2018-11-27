9c62937ea03546ec8f6160aa1f5eb34185774342
fiberleif <v-liguoq@microsoft.com>
2018-11-28 03:07:14 +0800

    Modify runall script

--- runall.sh
+++ runall.sh
@@ -1,9 +1,14 @@
 # #!/bin/bash
 
+# 7.1
 python code/main.py -n NIST36 -m FCN
 python code/main.py -n MNIST -m CNN
 python code/main.py -n NIST36 -m CNN
 python code/main.py -n EMNIST -m CNN
+# 7.2
+python code/main.py -n flowers17 -p True
+python code/main.py -n flowers17 -m CNN
+# plot results in /log
 python code/plot.py
 mkdir ./figure
 mv *.png ./figure/
